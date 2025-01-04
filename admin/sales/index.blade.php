e1f0d39585ead3122bd014d986840f1270aa2bef
EliteTools <elitetoolsife@gmail.com>
2025-01-04 12:35:40 +0100

    Update index.blade.php

--- admin/sales/index.blade.php
+++ admin/sales/index.blade.php
@@ -1,3 +1,4 @@
+
 <?php
 // Include necessary files
 include "header.php";
@@ -11,7 +12,42 @@ $myid = mysqli_real_escape_string($dbcon, $id);
 $get = mysqli_query($dbcon, "SELECT * FROM reports WHERE id='$myid'");
 $row = mysqli_fetch_assoc($get);
 ?>
+<?php
+ ob_start();
+ date_default_timezone_set('GMT');
 
+session_start();
+include "../includes/config.php";
+
+
+$id = $_GET['id'];
+$myid = mysqli_real_escape_string($dbcon, $id);
+
+
+$get = mysqli_query($dbcon, "SELECT * FROM reports WHERE id='$myid'");
+$row = mysqli_fetch_assoc($get);
+
+$date = date("Y-m-d h:i:sa");
+$reseller_id = $row['reseller_id'];
+$buyer = $row['user_id'];
+$acctype = $row['account_type'];
+$sid = $row['order_id'];
+$price = $row['price'];
+$d = $row['report_date'];
+// Check connection
+   $msg     = '
+  <div class="panel panel-default">
+  <div class="panel-body">
+<b>Refund request has been rejected.<br>Thank you for using Jerux.</b>
+ </div>
+  <div class="panel-footer"><div class="label label-danger">Admin</div> - '.date("d/m/Y h:i:s a").'</div>
+  </div>
+  ';
+  $date = date("d/m/Y h:i:s a");
+  $qq = mysqli_query($dbcon, "UPDATE reports SET memo = CONCAT(memo,'$msg'),status = ('0'),seen='1',lastreply='Admin',lastup='$date',state='rejected' WHERE id='$myid'")or die("mysql error");
+  header("location: viewr.php?id=$myid"); 
+
+?>
 <div id="page-content-wrapper">
     <div class="container-fluid">
         <div id="divPage">
