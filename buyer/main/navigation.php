bd41aa480ec1f2a0ccc7afb828029afe63867f07
EliteTools <elitetoolsife@gmail.com>
2025-01-25 21:25:47 +0100

    Create navigation.php

---
+++ buyer/main/navigation.php
@@ -0,0 +1,42 @@
+
+        <!-- Main Navigation -->
+        <div id="main-navigation" class="d-none d-lg-block mt-2 mt-lg-0">
+          <ul class="nav-main nav-main-dark nav-main-horizontal nav-main-hover">
+            <ul class="nav-main nav-main-horizontal nav-main-hover nav-main-dark">
+            <li class="nav-main-item">
+              <a class="nav-main-link active" href="index.html">
+                <i class="nav-main-link-icon si si-wallet"></i>
+                <span class="nav-main-link-name">Balance</span>
+                <span class="nav-main-link-badge badge rounded-pill bg-primary">0 $</span>
+              </a>
+            </li>
+            <li class="nav-main-heading">Manage</li>
+            <li class="nav-main-item">
+              <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
+                <i class="nav-main-link-icon fa fa-briefcase"></i>
+                <span class="nav-main-link-name">Products</span>
+              </a>
+              <ul class="nav-main-submenu">
+                <li class="nav-main-item">
+                  <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true" aria-expanded="false" href="#">
+                    <i class="nav-main-link-icon fa fa-server"></i>
+                    <span class="nav-main-link-name">Hostrs</span>
+                  </a>
+                  <ul class="nav-main-submenu">
+                    <li class="nav-main-item">
+                      <a class="nav-main-link" href="../rdp/index.html">
+                        <i class="nav-main-link-icon fa fa-desktop"></i>
+                        <span class="nav-main-link-name">Remote Desktop</span>
+                        <span class="nav-main-link-badge badge rounded-pill bg-primary">1</span>
+
+                    </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <!-- END Main Navigation -->
+      </div>
+    </div>
+  </div>
+           
