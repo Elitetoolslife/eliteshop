fe0c5d88f24bc57bdaca87415b34954325ce08bf
EliteTools <elitetoolsife@gmail.com>
2025-01-26 16:24:14 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,36 +1,8 @@
 <?php
-implement .env 
-//config/database.php
-
-$dbcon = mysqli_connect("localhost","artmir","Omeri1233","artmir") or die('enable connection'); 
-
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for database operations. This is
-    | the connection which will be utilized unless another connection
-    | is explicitly specified when you execute a query / statement.
-    |
-    */
-
     'default' => getenv('DB_CONNECTION') ?: 'sqlite',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Below are all of the database connections defined for your application.
-    | An example configuration is provided for each database system which
-    | is supported. You're free to add / remove connections.
-    |
-    */
 
     'connections' => [
 
@@ -111,38 +83,14 @@ return [
             'charset' => getenv('DB_CHARSET') ?: 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => getenv('DB_ENCRYPT') ?: 'yes',
-            // 'trust_server_certificate' => getenv('DB_TRUST_SERVER_CERTIFICATE') ?: 'false',
         ],
 
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
-    */
 
     'migrations' => [
         'table' => 'migrations',
         'update_date_on_publish' => true,
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
-    */
 
     'redis' => [
 
